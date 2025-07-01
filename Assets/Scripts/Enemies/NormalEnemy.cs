876b2bee7af4db4256daf6742ee6bf87fbbee250
Sincer1ty <woodsy0417@gmail.com>
2025-07-01 17:44:26 +0900

    Add EnemyBase Script

---
+++ Assets/Scripts/Enemies/NormalEnemy.cs
@@ -0,0 +1,11 @@
+namespace Enemies
+{
+    public class NormalEnemy : EnemyBase
+    {
+        public override EnemyType Type => EnemyType.Normal;
+        
+        // public float Hp => 10f;
+        
+        // public override void Attack() {}
+    }
+}
