9fcbe3e1f6de6a43b5b1dc95da9650deb6ab8008
jangwoo0305 <tjwkddn970305@gmail.com>
2025-07-30 20:23:44 +0900

    UI반복문

--- Assets/Scripts/UIMainMenu.cs
+++ Assets/Scripts/UIMainMenu.cs
@@ -8,34 +8,35 @@ public class UIMainMenu : MonoBehaviour
     public GameObject mainUI;
     public GameObject optionUI;
 
+    private GameObject[] allPanels;
+
     private void Start()
     {
-        startUI.SetActive(true);
+        allPanels = new GameObject[] { startUI, mainUI };
+        ShowStartUI(); // 시작 시 Start UI만 보이게
     }
 
-    // UI (Panel)
-    // 반복문으로 돌린다 : 하나만 키고 다 false
-    
-    // POPUP : 스택
-    
+    private void ShowOnly(GameObject targetPanel)
+    {
+        for (int i = 0; i < allPanels.Length; i++)
+        {
+            GameObject panel = allPanels[i];
+            panel.SetActive(panel == targetPanel); // 다를 때는 false를 반환
+        }
+    }
+
     public void ShowStartUI()
     {
-        startUI.SetActive(true);
-        mainUI.SetActive(false);
-        optionUI.SetActive(false);
+        ShowOnly(startUI);
     }
 
     public void ShowMainUI()
     {
-        mainUI.SetActive(true);
-        startUI.SetActive(false);
-        // optionUI.SetActive(false);
+        ShowOnly(mainUI);
     }
 
     public void ShowOptionUI()
     {
-        // mainUI.SetActive(false);
-        // startUI.SetActive(true);
         optionUI.SetActive(true);
     }
 
@@ -45,8 +46,10 @@ public class UIMainMenu : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-            Application.Quit();
+        Application.Quit();
 #endif
-
     }
 }
+
+// OptionUI전용 스크립트 만들고 hideoptionui는 전용스크립트로 보내고
+
