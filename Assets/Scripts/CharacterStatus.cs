2f906cc6b56dc35de591af72cb800a9e6ba0cbb7
jangwoo0305 <tjwkddn970305@gmail.com>
2025-08-17 15:58:21 +0900

    add int,hp,mp increase,decrease + show UI

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -34,13 +34,61 @@ public class CharacterStatus : MonoBehaviour
         UpdateUI();
     }
 
+    public void IncreaseIntelligence()
+    {
+        if (currentStat == null) return;
+        currentStat.intelligence = Mathf.Min(currentStat.intelligence + 1, 20);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
+    public void DecreaseIntelligence()
+    {
+        if (currentStat == null) return;
+        currentStat.intelligence = Mathf.Max(currentStat.intelligence - 1, 5);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
+    public void IncreaseHp()
+    {
+        if (currentStat == null) return;
+        currentStat.hp = Mathf.Min(currentStat.hp + 10, 200);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
+    public void DecreaseHp()
+    {
+        if (currentStat == null) return;
+        currentStat.hp = Mathf.Max(currentStat.hp - 10, 100);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
+    public void IncreaseMp()
+    {
+        if (currentStat == null) return;
+        currentStat.mp = Mathf.Min(currentStat.mp + 5, 150);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
+    public void DecreaseMp()
+    {
+        if (currentStat == null) return;
+        currentStat.mp = Mathf.Max(currentStat.mp - 5, 100);
+        CharacterDataManager.Save(currentStat);
+        UpdateUI();
+    }
+
     private void UpdateUI()
     {
         if  (currentStat == null) return;
         strengthText.text = currentStat.strength.ToString();
-        // intelligenceText.text = currentStat.intelligence.ToString();
-        // hpText.text = currentStat.hp.ToString();
-        // mpText.text = currentStat.mp.ToString();
+        intelligenceText.text = currentStat.intelligence.ToString();
+        hpText.text = currentStat.hp.ToString();
+        mpText.text = currentStat.mp.ToString();
     }
     
     public void ResetStat()
